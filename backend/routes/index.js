0bf036a6aad0566adf5bbe2b8a4d63c68a3c704b
Tamara <jermanust88@gmail.com>
2023-08-01 23:48:50 -0400

    router configuration setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,8 @@
 const express = require('express');
+const apiRouter = require('./api');
+
 const router = express.Router();
 
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-        'XSRF-Token': csrfToken
-    });
-});
+router.use('/api', apiRouter);
 
 module.exports = router;
