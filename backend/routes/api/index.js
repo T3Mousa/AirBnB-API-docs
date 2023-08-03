2170658f205dfcd1ad97d9150e38e225aa15a4c6
Tamara <jermanust88@gmail.com>
2023-08-03 00:07:55 -0400

    added user authentication express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,16 +1,7 @@
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth.js');
 
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
-
-router.get("/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-        'XSRF-Token': csrfToken
-    });
-});
+router.use(restoreUser);
 
 
 module.exports = router;
