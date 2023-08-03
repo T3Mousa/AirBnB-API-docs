106323781d351f839068aff00e9a235dac510c48
Tamara <jermanust88@gmail.com>
2023-08-03 00:22:03 -0400

    readded GET csrf token to routes/api/index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,5 +3,16 @@ const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
+router.post('/test', function (req, res) {
+    res.json({ requestBody: req.body });
+});
+
+router.get("/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-Token': csrfToken
+    });
+});
 
 module.exports = router;
