b9e32d7700f253ee42f64c7a82534fd129094813
Tamara <jermanust88@gmail.com>
2023-08-16 01:09:30 -0400

    added options into migrations

--- backend/db/migrations/20230810022941-create-spot.js
+++ backend/db/migrations/20230810022941-create-spot.js
@@ -67,7 +67,8 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
+
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Spots');
