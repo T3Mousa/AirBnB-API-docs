bc6150b2905e125ed847959e465bbbc827bc8cba
Tamara <jermanust88@gmail.com>
2023-08-17 01:32:29 -0400

    added query filters route to get all spots

--- backend/routes/api/validators.js
+++ backend/routes/api/validators.js
@@ -1,5 +1,6 @@
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation')
+// const { query } = require('express-validator/check')
 
 
 
@@ -18,11 +19,11 @@ const validateSpotParams = [
         .withMessage('Country is required'),
     check('lat')
         .exists({ checkFalsy: true })
-        .isNumeric({ min: -90, max: 90 })
+        .isFloat({ min: -90, max: 90 })
         .withMessage('Latitude is not valid'),
     check('lng')
         .exists({ checkFalsy: true })
-        .isNumeric({ min: -180, max: 180 })
+        .isFloat({ min: -180, max: 180 })
         .withMessage('Longitude is not valid'),
     check('name')
         .exists({ checkFalsy: true })
@@ -50,7 +51,44 @@ const validateReviewParams = [
 ];
 
 
+const validateSpotQueryParams = [
+    check('page')
+        .optional()
+        .isInt({ min: 1 })
+        .withMessage("Page must be greater than or equal to 1"),
+    check('size')
+        .optional()
+        .isInt({ min: 1 })
+        .withMessage("Size must be greater than or equal to 1"),
+    check('minLat')
+        .optional()
+        .isFloat({ min: -90 })
+        .withMessage("Minimum latitude is invalid"),
+    check('maxLat')
+        .optional()
+        .isFloat({ max: 90 })
+        .withMessage("Maximum latitude is invalid"),
+    check('minLng')
+        .optional()
+        .isFloat({ min: -180 })
+        .withMessage("Minimum longitude is invalid"),
+    check('maxLng')
+        .optional()
+        .isFloat({ max: 180 })
+        .withMessage("Maximum longitude is invalid"),
+    check('minPrice')
+        .optional()
+        .isFloat({ min: 0 })
+        .withMessage("Minimum price must be greater than or equal to 0"),
+    check('maxPrice')
+        .optional()
+        .isFloat({ min: 0 })
+        .withMessage("Maximum price must be greater than or equal to 0"),
+    handleValidationErrors
+];
+
 module.exports = {
     validateReviewParams,
-    validateSpotParams
+    validateSpotParams,
+    validateSpotQueryParams
 }
