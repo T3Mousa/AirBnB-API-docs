5e46175ab7e2aa02573af3da6db4d9bcb6aba9dc
Tamara <jermanust88@gmail.com>
2023-09-18 23:21:54 -0400

    Navigation and ProfileButton Components added

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import * as sessionActions from '../../store/session';
+import './Navigation.css';
 
 function Navigation({ isLoaded }) {
-    const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
-
-    const logout = (e) => {
-        e.preventDefault();
-        dispatch(sessionActions.logout());
-    };
 
     let sessionLinks;
     if (sessionUser) {
         sessionLinks = (
             <li>
                 <ProfileButton user={sessionUser} />
-                <button onClick={logout}>Log Out</button>
             </li>
         );
     } else {
