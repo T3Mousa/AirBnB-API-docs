9425c0fd32960b099a67bffd5a8815f436ca7c6d
Tamara <jermanust88@gmail.com>
2023-09-28 00:35:58 -0400

    fixed bugs with create a spot

--- frontend/src/components/Reviews/ReviewHeading.js
+++ frontend/src/components/Reviews/ReviewHeading.js
@@ -3,16 +3,15 @@ import { useDispatch } from 'react-redux';
 
 function ReviewHeading({ user, reviews, spot, spotReviewedByUser, spotOwnedByUser }) {
 
-    if (reviews) {
-        return (
-            <div className='reviewHeading'>
-                <h2>
-                    {spot.avgStarRating && <i className="fa-solid fa-star"></i>} {spot.avgStarRating} &middot; {spot.numReviews} review{spot.numReviews === 1 ? "" : "s"}
-                </h2>
-                {user && !spotReviewedByUser && !spotOwnedByUser && <button className="createReviewButton">Post Your Review</button>}
-            </div>
-        )
-    }
+    return (
+        <div className='reviewHeading'>
+            <h2><i className="fa-solid fa-star"></i>
+                {!reviews && "New"}
+                {spot.avgStarRating ? spot.avgStarRating : "New"} &middot; {spot.avgStarRating ? spot.numReviews : ""} {spot.numReviews && "reviews"}
+            </h2>
+            {reviews && user && !spotReviewedByUser && !spotOwnedByUser && <button className="createReviewButton">Post Your Review</button>}
+        </div>
+    )
     if (!reviews) {
         return (
             <div className='reviewHeading'>
